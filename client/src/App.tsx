768b2f3c17dc98d633357cdd9333a12f33d9fb9e
m_khorosh <marina.khoroshilova@gmail.com>
2022-04-02 20:48:35 +0300

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    const title: string = 'School LMS System';
+    return (
+        <div className="App">
+            <h1>{title}</h1>
+        </div>
+    );
+}
+
+export default App;
